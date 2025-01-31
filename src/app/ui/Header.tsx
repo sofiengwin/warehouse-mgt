3ef1a9d6e5ff9aa899838b2f7e7eac59285e0a72
sofiengwin <sengodwin@gmail.com>
2025-01-31 10:51:41 -0800

    Remove unused dependencies

--- src/app/ui/Header.tsx
+++ src/app/ui/Header.tsx
@@ -7,12 +7,12 @@ export default function Header() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home">Warehouse Mgt</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/">Dashboard</Nav.Link>
-            <Nav.Link href="/orders">Orders</Nav.Link>
+            <Nav.Link href="/">Picking List</Nav.Link>
+            <Nav.Link href="/orders">Packing List</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
